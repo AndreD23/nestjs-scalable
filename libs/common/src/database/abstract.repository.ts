1db98ff2125462ffecefb699e71d9eda97632335
André Dorneles <andre@imperiosoft.com.br>
2024-07-28 12:39:20 -0300

    Remove CommonModule and introduce DatabaseModule
    CommonModule and its related services and tests were removed. DatabaseModule with abstract schema and repository functionality was added to handle database operations. AppModule has been updated to import DatabaseModule.

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,71 @@
+import { AbstractSchema } from '@app/common/database/abstract.schema';
+import { FilterQuery, Model, UpdateQuery } from 'mongoose';
+import { Logger, NotFoundException } from '@nestjs/common';
+
+export abstract class AbstractRepository<TDocument extends AbstractSchema> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: undefined,
+    });
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model
+      .findOne(filterQuery)
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, { new: true })
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findAll(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>(true);
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndDelete(filterQuery)
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn(
+        `Document not found with filter: ${JSON.stringify(filterQuery)}`,
+      );
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+}
---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  ConfigModule as NestjsConfigModule,
+  ConfigService,
+} from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [
+        NestjsConfigModule.forRoot({
+          validationSchema: Joi.object({
+            MONGODB_URI: Joi.string().required(),
+          }),
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
